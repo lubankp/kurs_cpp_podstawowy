44a5c2595a4fb5db966f7835f0d70ae3e495629f
lubankp <66789546+lubankp@users.noreply.github.com>
2020-06-11 18:50:01 +0200

    Update task2.cpp

--- module1/task2.cpp
+++ module1/task2.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-// Write your function here
+int max(int number_1, int number_2, int number_3){
+  int max;
+  if ((number_1 > number_2) and (number_1 > number_3)){
+    max=number_1;
+  }else if(number_2 > number_3){
+    max=number_2;
+  }else{
+    max=number_3;
+  }
+  return max;
+}
 
 int main() {
     std::cout << "max (1, 2, 3): " << max(1, 2, 3) << "\n";
